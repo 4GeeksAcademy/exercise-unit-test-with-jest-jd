314bfc414d29e7bdc8cb0c87321713f74d8f6e31
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-06-24 17:42:46 +0000

    agregados el resto de test

--- test.js
+++ test.js
@@ -1,19 +1,27 @@
-const {sum}=  require("./app.js");
+
+const { sum,fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
+const { test, expect } = require('@jest/globals');
+
 test ("adds 14+9 to equal 23",()=>{
-   let total = sum (14,9);
-   expect(total).toBe(23);
-})
+    let total = sum (14,9);
+    expect(total).toBe(23);
+ })
 
 test("One euro should be 1.07 dollars", function() {
-    // Importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js');
-
-    // Uso la función como debe ser usada
     const dollars = fromEuroToDollar(3.5);
-
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
     const expected = 3.5 * 1.07;
 
-    // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); 
+})
+
+test("convert the value of dollar to yen", function () {
+    const yenes = fromDollarToYen(1);
+    const expected = (1 / 1.07) * 156.5;
+    expect(fromDollarToYen(1)).toBe(expected);
+})
+
+test("convert the value of yenes to pound", function () {
+    const pound = fromYenToPound(1);
+    const expected = (1 / 156.5) * 0.87;
+    expect(fromYenToPound(1)).toBe(expected);
 })
